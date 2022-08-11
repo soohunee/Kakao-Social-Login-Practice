c9463d17eaefa8bc9d2a044a979287fe30771049
soohunee <tngjsdl036@gmail.com>
2022-08-12 01:42:22 +0900

    Initialize Project

---
+++ src/main/java/com/kakao/socialLogin/practice/kakaoSocialLogin/KakaoSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.socialLogin.practice.kakaoSocialLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoSocialLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/socialLogin/practice/kakaoSocialLogin/KakaoSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.socialLogin.practice.kakaoSocialLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
